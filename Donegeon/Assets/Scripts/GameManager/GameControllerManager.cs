82b25adcf81b2ddd026c487da13b2b9e9b0b69ed
Aluxne <jtongpakdee@gmail.com>
2023-01-20 15:41:39 +0700

    Build Fix

--- Donegeon/Assets/Scripts/GameManager/GameControllerManager.cs
+++ Donegeon/Assets/Scripts/GameManager/GameControllerManager.cs
@@ -3,8 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
-using UnityEditor.Animations;
-using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
